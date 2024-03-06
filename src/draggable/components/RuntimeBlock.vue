3077c1239e9ef9e48f15784581faec873a5d09aa
lizw <1183409807@qq.com>
2024-03-06 10:02:18 +0800

    ...

--- src/draggable/components/RuntimeBlock.vue
+++ src/draggable/components/RuntimeBlock.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+// 定义组件选项
+import { Fragment } from "vue";
+import { DesignBlock } from "@/draggable/types/DesignBlock";
+import { createBlockComponent } from "@/draggable/BlockFactory";
 
+defineOptions({
+    name: 'RuntimeBlock',
+});
+
+// 定义 Props 类型
+interface RuntimeBlockProps {
+    block?: DesignBlock;
+}
+
+// 读取组件 props 属性
+const props = withDefaults(defineProps<RuntimeBlockProps>(), {});
+
+// 动态创建 Block 组件
+const BlockComponent = props.block ? createBlockComponent(props.block) : Fragment;
 </script>
 
 <template>
-
+    <BlockComponent v-if="props.block"/>
 </template>
 
 <style scoped>
--- src/views/Page02.vue
+++ src/views/Page02.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import RuntimeBlock from "@/draggable/components/RuntimeBlock.vue";
+import { block01 } from "./Block01";
+import { block02 } from "./Block02";
+
+const show = ref(1);
+const block01ref = ref();
 
 </script>
 
 <template>
     Page02
+    <button @click="show++">{{ show % 2 === 0 ? '卸载' : '加载' }}</button>
+
+    <div style="height: 12px;"/>
+    <div style="width:300px;border: 1px solid #ccc;">
+        空组件
+        <RuntimeBlock/>
+    </div>
+
+    <div style="height: 12px;"/>
+    <RuntimeBlock ref="block01ref" v-if="show%2===0" :block="block01"/>
+
+    <div style="height: 12px;"/>
+    <RuntimeBlock v-if="show%2===0" :block="block01"/>
+
+    <div style="height: 12px;"/>
+    <RuntimeBlock :block="block02"/>
 </template>
 
 <style scoped>
