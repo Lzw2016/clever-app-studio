c670e1070f86635edb9eb15fa0bd7c0c2c5c4f67
lizw <1183409807@qq.com>
2024-04-22 18:39:44 +0800

    ...

--- src/draggable/register/meta/Button.ts
+++ src/draggable/register/meta/Button.ts
@@ -169,137 +169,16 @@ export default defineComponentMeta({
                 },
             ],
         },
-        // events: {
-        //     title: "",
-        //     groups: [
-        //         {
-        //             title: "组件事件",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "HTML事件",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //     ],
-        // },
-        // style: {
-        //     title: "",
-        //     groups: [
-        //         {
-        //             title: "布局",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "间距",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "尺寸",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "定位",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "文本",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "背景",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "边框",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "效果",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //     ],
-        // },
-        // advanced: {
-        //     title: "",
-        //     groups: [
-        //         {
-        //             title: "指令",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             title: "权限",
-        //             items: [
-        //                 {
-        //                     cmp: "TextSetter",
-        //                     propsName: "aaa",
-        //                 },
-        //             ],
-        //         },
-        //     ],
-        // },
+        events: {
+            title: "",
+            groups: [],
+        },
+        style: {
+        },
+        advanced: {
+            title: "",
+            groups: [],
+        },
     },
     placeholder: {},
     i18n: {},
