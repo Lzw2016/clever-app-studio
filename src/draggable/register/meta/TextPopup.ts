698a9a05e67c4127db8c9a8abc7b05763da5b85a
lizw <1183409807@qq.com>
2024-08-24 13:12:23 +0800

    ...

--- src/draggable/register/meta/TextPopup.ts
+++ src/draggable/register/meta/TextPopup.ts
@@ -7,34 +7,63 @@ export default defineComponentMeta({
     name: "输入框",
     description: "",
     version: "0.0.1",
-    docLink: "",
+    docLink: "https://opentiny.design/tiny-vue/zh-CN/os-theme/components/text-popup",
     icon: createVNode(TextPopupSvg, { 'stroke-width': "2", style: { width: "20px", height: "20px" } }),
     defDesignNode: {
         props: {
-            style: {},
+            placeholder: "请输入",
         },
     },
     setter: {
         props: {
+            enableVModel: true,
+            modelValueSetter: {
+                cmp: "StringSetter",
+            },
             groups: [
                 {
                     title: "常用",
                     items: [
-
+                        {
+                            cmp: "StringSetter",
+                            label: "占位文本",
+                            propsName: "placeholder",
+                        },
+                        {
+                            cmp: "StringSetter",
+                            label: "分隔符",
+                            propsName: "separtor",
+                        },
+                        {
+                            cmp: "BoolSetter",
+                            label: "是否只读",
+                            propsName: "readonly",
+                        },
                     ],
                 },
                 {
                     title: "风格",
-                    items: [],
+                    items: [
+                        {
+                            cmp: "NumberSetter",
+                            cmpProps: {
+                                step: 1,
+                                unit: "px",
+                            },
+                            label: "宽度",
+                            propsName: "width",
+                        },
+                    ],
                 },
             ],
         },
         events: {
+            includeInnerEvents: true,
+            excludeInnerEvents: ["表单事件"],
             groups: [],
         },
         style: {},
-        advanced: {
-        },
+        advanced: {},
     },
     placeholder: {},
     i18n: {},
