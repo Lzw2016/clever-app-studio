584d3fc27472923607ba8f8a12c2725f803730d9
lizw <1183409807@qq.com>
2024-08-25 16:19:16 +0800

    ...

--- src/draggable/register/meta/ScrollText.ts
+++ src/draggable/register/meta/ScrollText.ts
@@ -7,11 +7,11 @@ export default defineComponentMeta({
     name: "文字滚动",
     description: "",
     version: "0.0.1",
-    docLink: "",
+    docLink: "https://opentiny.design/tiny-vue/zh-CN/os-theme/components/scroll-text",
     icon: createVNode(ScrollTextSvg, { 'stroke-width': "2", style: { width: "20px", height: "20px" } }),
     defDesignNode: {
         props: {
-            style: {},
+            text: "这是一段滚动的文字",
         },
     },
     setter: {
@@ -20,7 +20,44 @@ export default defineComponentMeta({
                 {
                     title: "常用",
                     items: [
-
+                        {
+                            cmp: "StringSetter",
+                            label: "文本内容",
+                            propsName: "text",
+                            defPropsValue: "ScrollText",
+                        },
+                        {
+                            cmp: "SelectSetter",
+                            cmpProps: {
+                                options: [
+                                    { value: "up", label: "向上" },
+                                    { value: "left", label: "向左" },
+                                    { value: "down", label: "向下" },
+                                    { value: "right", label: "向右" },
+                                ],
+                            },
+                            label: "滚动方向",
+                            propsName: "direction",
+                            defPropsValue: "left",
+                        },
+                        {
+                            cmp: "NumberSetter",
+                            cmpProps: {
+                                step: 1,
+                                unit: "秒",
+                            },
+                            label: "滚动时间",
+                            labelTips: "整个滚动过程所用的时间",
+                            propsName: "time",
+                            defPropsValue: 5,
+                        },
+                        {
+                            cmp: "BoolSetter",
+                            label: "自动停止",
+                            labelTips: "hover 滚动是否停止",
+                            propsName: "hoverStop",
+                            defPropsValue: true,
+                        },
                     ],
                 },
                 {
@@ -30,12 +67,16 @@ export default defineComponentMeta({
             ],
         },
         events: {
+            includeInnerEvents: true,
+            excludeInnerEvents: ["表单事件"],
             groups: [],
         },
         style: {},
-        advanced: {
-        },
+        advanced: {},
     },
-    placeholder: {},
+    placeholder: {
+        // 默认插槽，滚动的内容的插槽
+        // default
+    },
     i18n: {},
 });
